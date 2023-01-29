1947aff9d634348e06e9b844c0992b4ed2bf7f20
mishanwashere <mishangroenewald@gmail.com>
2023-01-29 16:02:08 +0200

    feat(app.ts): validate csv data entries, store validated entries to Set data structures, added relevant logging.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { createReadStream } from 'node:fs';
 import * as readline from 'node:readline/promises';
 
 import { LoggingService } from './services/logging.service.js';
+import { validateDataForSpecialCharactersAndNumbers, validateDataForAlphabeticalCharactersOnly } from './helpers/validate-strings.js';
 
 const logging = new LoggingService();
 
@@ -9,7 +10,7 @@ let malePlayers: Set<string> = new Set();
 let femalePlayers: Set<string> = new Set();
 
 (async () => {
-try {
+  try {
     const fileDataStream = await createReadStream('data.csv', { encoding: 'utf8' });
   
     const readLine = readline.createInterface({
@@ -18,7 +19,7 @@ try {
     });
   
     for await (const dataEntry of readLine) {
-      // file output per line.
+      validateDataEntryForSpecialCharactersAndNumbers(dataEntry);
     }
   } catch (error) {
     console.error("Failed to read data :: " + error);
@@ -29,4 +30,62 @@ try {
   console.log(femalePlayers);
 });
 
+function splitDataByGender(dataEntry: string): void {
+  try {
+    if (dataEntry.includes(',f')) {
+      let strippedGenderDataEntry: Array<string> = dataEntry.split(','); // remove gender from data set.
+      if (!validateDataForAlphabeticalCharactersOnly(strippedGenderDataEntry[0])) {
+        throw `Data Entry contains special characters or numbers. :: ${strippedGenderDataEntry[0]}`;
+      }
+
+      saveToFemaleDataSet(strippedGenderDataEntry[0]);
+      return;
+    } 
+  
+    if (dataEntry.includes(',m')) {
+      let strippedGenderDataEntry: Array<string> = dataEntry.split(','); // remove gender from data set.
+      if (!validateDataForAlphabeticalCharactersOnly(strippedGenderDataEntry[0])) {
+        throw `Data Entry contains special characters or numbers. :: ${strippedGenderDataEntry[0]}`;
+      }
+
+      saveToMaleDataSet(strippedGenderDataEntry[0]);
+      return;
+    }
+
+    throw `No Gender Supplied on data entry :: ${dataEntry}`;
+  } catch(error) {
+    console.error(error);
+    logging.log("Fatal", error);
+  }
+}
+
+function validateDataEntryForSpecialCharactersAndNumbers(dataEntry: string): void {
+  try {
+    if (!validateDataForSpecialCharactersAndNumbers(dataEntry)) {
+      throw `Data Entry contains special characters or numbers. :: ${dataEntry}`;
+    }
+
+    splitDataByGender(dataEntry);
+  } catch(error) {
+    console.error(error);
+    logging.log("Fatal", error);
+  }
+}
+
+function saveToFemaleDataSet(strippedGenderDataEntry: string): void {
+  if (femalePlayers.has(strippedGenderDataEntry)) {
+    console.warn(`Duplicate Entry :: ${strippedGenderDataEntry}`);
+    logging.log("Warning", `Duplicate Entry :: ${strippedGenderDataEntry}`);
+  }
+
+  femalePlayers.add(strippedGenderDataEntry);
+}
+
+function saveToMaleDataSet(strippedGenderDataEntry: string): void {
+  if (malePlayers.has(strippedGenderDataEntry)) {
+    console.warn(`Duplicate Entry :: ${strippedGenderDataEntry}`);
+    logging.log("Warning", `Duplicate Entry :: ${strippedGenderDataEntry}`);
+  }
+
+  malePlayers.add(strippedGenderDataEntry);
 }
